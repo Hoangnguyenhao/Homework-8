fa5f07b388f8e07b2a9cdf4e44c41039bab45ade
Hoangnguyenhao <tranhoangnguyenhao2003@gmail.com>
2025-03-20 17:09:04 +0700

    Add files via upload

---
+++ baitapAdvance/Infrastructure/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Infrastructure;
+
+public interface IRepository<T> where T : class
+{
+    void Add(T entity);
+    void Remove(T entity);
+    IEnumerable<T> GetAll();
+    T GetById(int id);
+}
+
